fccd1d4e84052e7bf555532a4332c7a603e4b0d8
samlich <1349989+samlich@users.noreply.github.com>
2021-02-05 17:05:59 +0000

    package.json: fix engine.peertube key

--- client/common.js
+++ client/common.js
@@ -1,7 +1,7 @@
 // import marked from 'marked' - doesn't work in main.js
 const marked = require('marked')
 
-const version = '0.1.0'
+const version = '0.1.1'
 const tableOfContentsField = 'table-of-contents'
 
 /*
